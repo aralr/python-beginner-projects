b946501829a23b9fc439b86c43a3a666a55c3b22
Arantza <ara.lr0104@gmail.com>
2025-12-30 14:07:46 -0600

    Create fortune_cookie.py

---
+++ fortune_cookie.py
@@ -0,0 +1,17 @@
+import random
+
+def fortune():
+
+  fortunes = [
+    'Dont pursue happiness – create it.',
+    'All things are difficult before they are easy.',
+    'The early bird gets the worm, but the second mouse gets the cheese.',
+    'Someone in your life needs a letter from you.',
+    'Dont just think. Act!',
+    'Your heart will skip a beat.',
+    'The fortune you search for is in another cookie.',
+    'Help! Im being held prisoner in a Chinese bakery!'
+  ]
+
+  return random.choice(fortunes)
+print(fortune())
